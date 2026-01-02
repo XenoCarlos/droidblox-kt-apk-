36678ffead2019a636a4fe79818b0ffc65d12a57
ekard <105611677+helloplauz10@users.noreply.github.com>
2026-01-02 10:15:40 +0800

    add fflags screen

---
+++ app/src/main/java/com/drake/droidblox/ui/view/FFlagsScreen.kt
@@ -0,0 +1,14 @@
+package com.drake.droidblox.ui.view
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import com.drake.droidblox.ui.components.BasicScreen
+
+@Composable
+fun FFlagsScreen(
+    navController: NavController? = null
+) {
+    BasicScreen("Fast Flags", navController) {
+
+    }
+}
