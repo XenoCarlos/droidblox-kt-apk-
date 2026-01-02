278013a63d632e6ed43da3bf15808b0a54bd8939
ekard <105611677+helloplauz10@users.noreply.github.com>
2026-01-03 00:50:06 +0800

    add extended dropdown

--- app/src/main/java/com/drake/droidblox/ui/components/ExtendedDropdown.kt
+++ app/src/main/java/com/drake/droidblox/ui/components/ExtendedDropdown.kt
@@ -3,22 +3,85 @@ package com.drake.droidblox.ui.components
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
+data class DropdownItem(
+    val text: String,
+    val onClick: (() -> Unit) = {}
+)
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ExtendedDropdown(
     title: String,
-    subtitle: String
+    subtitle: String,
+    items: List<DropdownItem>,
+    default: String? = items[0].text
 ) {
+    var expanded by remember { mutableStateOf(false) }
+    var selectedOption by remember { mutableStateOf(items[0].text) }
+
     Row(
-        modifier = Modifier.fillMaxWidth(),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(5.dp),
         verticalAlignment = Alignment.CenterVertically
     ) {
         Column(modifier = Modifier.weight(1f)) {
             TitleWithSubtitle(title, subtitle)
         }
-        TODO("add dropdown here")
+        // https://stackoverflow.com/a/67111599
+        ExposedDropdownMenuBox(
+            expanded = expanded,
+            onExpandedChange = { expanded = !expanded },
+            modifier = Modifier.fillMaxWidth(0.4f)
+        ) {
+            OutlinedTextField(
+                modifier = Modifier
+                    .menuAnchor()
+                    .fillMaxWidth(),
+                value = selectedOption,
+                onValueChange = {},
+                readOnly = true,
+                trailingIcon = {
+                    ExposedDropdownMenuDefaults.TrailingIcon(
+                        expanded = expanded
+                    )
+                },
+                colors = ExposedDropdownMenuDefaults.textFieldColors()
+            )
+            ExposedDropdownMenu(
+                expanded = expanded,
+                onDismissRequest = {
+                    expanded = false
+                }
+            ) {
+                items.forEach {
+                    DropdownMenuItem(
+                        text = { Text(it.text) },
+                        onClick = {
+                            selectedOption = it.text
+                            it.onClick()
+                            expanded = false
+                        }
+                    )
+                }
+            }
+        }
     }
 }
