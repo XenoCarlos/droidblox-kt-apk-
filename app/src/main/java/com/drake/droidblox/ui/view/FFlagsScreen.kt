031ab99b02ed78c0908c5463ab538fa3c03d23d9
ekard <105611677+helloplauz10@users.noreply.github.com>
2026-01-02 12:40:51 +0800

    add fflags

--- app/src/main/java/com/drake/droidblox/ui/view/FFlagsScreen.kt
+++ app/src/main/java/com/drake/droidblox/ui/view/FFlagsScreen.kt
@@ -2,13 +2,55 @@ package com.drake.droidblox.ui.view
 
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
+import com.drake.droidblox.DBApplication
 import com.drake.droidblox.ui.components.BasicScreen
+import com.drake.droidblox.ui.components.ExtendedSwitch
+import com.drake.droidblox.ui.components.SectionText
 
 @Composable
 fun FFlagsScreen(
     navController: NavController? = null
 ) {
+    val settingsManager = DBApplication.instance.settingsManager
+    /*
+    follows the allowlist: https://devforum.roblox.com/t/allowlist-for-local-client-configuration-via-fast-flags/3966569
+    Geometry:
+
+    DFIntCSGLevelOfDetailSwitchingDistance
+    DFIntCSGLevelOfDetailSwitchingDistanceL12
+    DFIntCSGLevelOfDetailSwitchingDistanceL23
+    DFIntCSGLevelOfDetailSwitchingDistanceL34
+
+    Rendering:
+
+    DFFlagTextureQualityOverrideEnabled
+    DFIntTextureQualityOverride
+    FIntDebugForceMSAASamples
+    DFFlagDisableDPIScale
+    FFlagDebugSkyGray
+    DFFlagDebugPauseVoxelizer
+    DFIntDebugFRMQualityLevelOverride
+    FIntFRMMaxGrassDistance
+    FIntFRMMinGrassDistance
+    FFlagDebugGraphicsPreferVulkan
+    FFlagDebugGraphicsPreferOpenGL
+
+    User Interface:
+
+    FIntGrassMovementReducedMotionFactor
+
+    the excluded fflags for android will include the following:
+    FFlagHandleAltEnterFullscreenManually
+    FFlagDebugGraphicsPreferD3D11
+    */
     BasicScreen("Fast Flags", navController) {
+        ExtendedSwitch(
+            "Allow DroidBlox to manage Fast Flags",
+            "Disabling this will prevent anything configured here from being applied to Roblox.",
+            settingsManager.applyFFlags
+        ) { settingsManager.applyFFlags = it }
+        SectionText("Rendering")
+        TODO("Extended Dropdown Anti-aliasing quality (MSAA)")
 
     }
 }
