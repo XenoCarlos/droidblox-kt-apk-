8da77c101ebd9e91aeed7b4aef4839fb16921950
ekard <105611677+helloplauz10@users.noreply.github.com>
2026-01-18 21:12:42 +0800

    idk what the fuck i am doing

--- app/src/main/java/com/drake/droidblox/ui/view/navigation/AnimatedComposable.kt
+++ app/src/main/java/com/drake/droidblox/ui/view/navigation/AnimatedComposable.kt
@@ -3,34 +3,13 @@ package com.drake.droidblox.ui.view.navigation
 import androidx.compose.animation.AnimatedVisibilityScope
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.animation.scaleIn
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 
-//fun NavGraphBuilder.animatedComposable(
-//    route: String,
-//    content: @Composable AnimatedVisibilityScope.(NavBackStackEntry) -> Unit
-//) = composable(
-//    route = route,
-//    enterTransition = {
-//        fadeIn(animationSpec = tween(220, delayMillis = 90)) +
-//                scaleIn(
-//                    initialScale = 0.92f,
-//                    animationSpec = tween(220, delayMillis = 90)
-//                )
-//    },
-//    popEnterTransition = {
-//        fadeIn(animationSpec = tween(220, delayMillis = 90)) +
-//                scaleIn(
-//                    initialScale = 0.92f,
-//                    animationSpec = tween(220, delayMillis = 90)
-//                )
-//    },
-//    content = content
-//)
-private val animationSpec = tween<Float>(durationMillis = 400)
 fun NavGraphBuilder.animatedComposable(
     route: String,
     content: @Composable AnimatedVisibilityScope.(NavBackStackEntry) -> Unit
@@ -38,7 +17,17 @@ fun NavGraphBuilder.animatedComposable(
     route = route,
     enterTransition = {
         fadeIn(animationSpec = tween(300)) +
-        scaleIn(initialScale = 0.85f, animationSpec = tween(400))
+                scaleIn(initialScale = 0.85f, animationSpec = tween(400))
+    },
+    popEnterTransition = {
+        fadeIn(animationSpec = tween(300)) +
+                scaleIn(initialScale = 0.85f, animationSpec = tween(400))
+    },
+    exitTransition = {
+        fadeOut(animationSpec = tween(300))
+    },
+    popExitTransition = {
+        fadeOut(animationSpec = tween(300))
     },
     content = content
 )
